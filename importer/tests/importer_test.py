63dbe8254e812ccf979fdf4db97f03e3016b8a26
CharlesNunn001 <cn14@sanger.ac.uk>
2019-10-22 16:11:22 +0100

    RT671028 finished load tests

--- importer/tests/importer_test.py
+++ importer/tests/importer_test.py
@@ -1,35 +1,38 @@
 import unittest
 import os
+import glob
 from importer.importer import DataImporter
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 
+OUTPUT = 'base'
+TICKET = 123
+DATABASE = 'database'
 COMMAND_FILE_NAME = '.'
 COMMAND_1 = DataImporter('base/123', 123, 0, 'database')
 COMMAND_2 = DataImporter('base/123', 123, 1, 'database')
 COMMANDS = [COMMAND_1, COMMAND_2]
 
+def glob_replacer(ticket_string, recursive):
+    if ticket_string != f'/lustre/scratch118/infgen/pathogen/pathpipe/external_seq_data/{TICKET}/external_{TICKET}_*.xls':
+        raise Exception('Ticket lost in transfer.')
+    elif recursive != False:
+        raise Exception('Recursive not set to false.')
+    else:
+        GLOB_LIST = ['one', 'two']
+        return GLOB_LIST
+
 class importerTesting(unittest.TestCase):
 
     def test_importer_setup(self):
-        #TODO return glob as 2 item list -- compare the returned list to the initiated list -- assert glob called correctly
-        #with patch(importer.importer.glob.) as
-        # @staticmethod
-        # def new_instance(base: str, ticket: int, database: str):
-        #     loader_list = []
-        #     destination = "%s/%d" % (base, ticket)
-        #     for index, file in enumerate(glob.glob(
-        #             f"/lustre/scratch118/infgen/pathogen/pathpipe/external_seq_data/{ticket}/external_{ticket}_*.xls",
-        #             recursive=False)):
-        #         command = DataImporter(destination, ticket, index, database)
-        #         loader_list.append(command)
-        #     return loader_list
-        #
-        # def __init__(self, destination: str, ticket: int, index: int, database: str):
-        #     self.destination = destination
-        #     self.ticket = ticket
-        #     self.index = index
-        #     self.database = database
-        pass
+        with patch('glob.glob', side_effect=glob_replacer) as glob_mock:
+            RECEIVED_LIST = DataImporter.new_instance(OUTPUT, TICKET, DATABASE)
+            for MATCHING_COMMAND, CREATED_COMMAND in enumerate(RECEIVED_LIST):
+                self.assertEqual(CREATED_COMMAND.destination, COMMANDS[MATCHING_COMMAND].destination)
+                self.assertEqual(CREATED_COMMAND.database, COMMANDS[MATCHING_COMMAND].database)
+                self.assertEqual(CREATED_COMMAND.ticket, COMMANDS[MATCHING_COMMAND].ticket)
+                self.assertEqual(CREATED_COMMAND.index, COMMANDS[MATCHING_COMMAND].index)
+            glob_mock.assert_called_once_with('/lustre/scratch118/infgen/pathogen/pathpipe/external_seq_data/123/external_123_*.xls', recursive=False)
+
 
     def test_importer_printout(self):
         TESTER_LINES = ['\n',
