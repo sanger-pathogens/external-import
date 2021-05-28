c44ae327a49168fae70d45fcb8c9dcf807457b12
CharlesNunn001 <cn14@sanger.ac.uk>
2021-05-28 09:01:51 +0100

    Added openpyxl as reading tool for xlsx

--- importer/loader.py
+++ importer/loader.py
@@ -17,7 +17,7 @@ class SpreadsheetLoader:
             self._sheet = self._workbook.sheet_by_index(0)
             self._format = 'xls'
 
-    def load(self):
+    def load_xls(self):
         result = Spreadsheet()
         data_row = 0
         header_row = 0
@@ -33,7 +33,7 @@ class SpreadsheetLoader:
             if self._sheet.cell_value(i, 0) == 'Sequencing Technology':
                 result.technology = self._sheet.cell_value(i, 1)
             if self._sheet.cell_value(i, 0) == 'Study Accession number':
-                result.accession = self.__extract_text_value(i, 1)
+                result.accession = self.__extract_text_value_xls(i, 1)
             if self._sheet.cell_value(i, 0) == 'Total size of files in GBytes':
                 result.size = self._sheet.cell_value(i, 1)
             if self._sheet.cell_value(i, 0) == 'Data to be kept until':
@@ -67,25 +67,25 @@ class SpreadsheetLoader:
                 library_name_column = i
         reads = []
         for i in range(data_row, self._sheet.nrows):
-            sample_name = self.__extract_float_value(i, sample_name_column)
-            library_name = self.__extract_float_value(i, library_name_column)
+            sample_name = self.__extract_float_value_xls(i, sample_name_column)
+            library_name = self.__extract_float_value_xls(i, library_name_column)
             if library_name is None:
                 library_name = sample_name
             if filename_column is not None:
                 reads.append(RawRead(
-                    self.__extract_text_value(i, filename_column),
-                    self.__extract_text_value(i, mate_filename_column),
+                    self.__extract_text_value_xls(i, filename_column),
+                    self.__extract_text_value_xls(i, mate_filename_column),
                     sample_name,
-                    self.__extract_text_value(i, sample_accession_column),
-                    self.__extract_float_value(i, taxon_id_column),
+                    self.__extract_text_value_xls(i, sample_accession_column),
+                    self.__extract_float_value_xls(i, taxon_id_column),
                     library_name))
             if run_accession_column is not None:
                 reads.append(RawRead(
-                    (self.__extract_text_value(i, run_accession_column)),
-                    self.__extract_text_value(i, double_ended_reads_column),
+                    (self.__extract_text_value_xls(i, run_accession_column)),
+                    self.__extract_text_value_xls(i, double_ended_reads_column),
                     sample_name,
-                    self.__extract_text_value(i, sample_accession_column),
-                    self.__extract_float_value(i, taxon_id_column),
+                    self.__extract_text_value_xls(i, sample_accession_column),
+                    self.__extract_float_value_xls(i, taxon_id_column),
                     library_name))
         result.reads = reads
         return result
@@ -170,11 +170,11 @@ class SpreadsheetLoader:
             return self.__extract_text_value_xlsx(row, column)
         return str(int(self._sheet.cell(row=row, column=column).value))
 
-    def __extract_text_value(self, row, column):
+    def __extract_text_value_xls(self, row, column):
         new_data = self._sheet.cell_value(row, column).strip()
         return None if new_data == '' else new_data
 
-    def __extract_float_value(self, row, column):
+    def __extract_float_value_xls(self, row, column):
         if self._sheet.cell_type(row, column) != xlrd.XL_CELL_NUMBER:
-            return self.__extract_text_value(row, column)
+            return self.__extract_text_value_xls(row, column)
         return str(int(self._sheet.cell_value(row, column)))
